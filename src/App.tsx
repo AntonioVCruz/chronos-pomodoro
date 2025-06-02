4c37247c9b54c67f051da6de52865d512d6d9120
Antonio Cruz <vicentecruz027@gmail.com>
2025-06-02 19:18:52 -0300

    added containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import { TimerIcon } from 'lucide-react';
-import { Heading } from './components/Heading';
-
 import './styles/global.css'
 import './styles/theme.css'
+
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
     return ( 
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-            <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-        Culpa commodi sunt porro fuga aliquam hic dicta ullam sed autem sapiente inventore id, magnam nostrum minima dolor soluta cum! 
-        Aspernatur, maxime.
-      </p>
+        <Container>
+            <Heading>LOGO</Heading>
+        </Container>
+
+        <Container>
+            <Heading>MENU</Heading>
+        </Container>
     </>
     );
 }
